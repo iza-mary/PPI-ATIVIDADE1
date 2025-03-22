40f0d32c6b5ab4d1b78646fe379f46ce107395c7
Maria <marizasoares3422@gmail.com>
2025-03-22 18:12:52 -0300

    atualização

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const localhost = "0.0.0.0";
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: "m1Nh4Ch4v353cR3t4",
@@ -16,7 +16,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 15
     }
-}))
+}));
 
 app.get("/login", (requisicao, resposta) => {
     resposta.redirect('/login.html');
@@ -36,7 +36,7 @@ app.post("/login", (requisicao, resposta) => {
     const email = requisicao.body.email;
     const senha = requisicao.body.senha;
 
-    if (usuario === "admin" && email === "admin@email.com" && senha === "admin") {
+    if (usuario === "admin" && email === "admin@email.com" && senha === "admin123") {
         requisicao.session.autenticado = true;
         console.log("Usuário autenticado:", requisicao.session); // Verifica a sessão
         resposta.redirect('/detalhes.html');
@@ -46,12 +46,28 @@ app.post("/login", (requisicao, resposta) => {
     }
 });
 
-
 app.use(express.static('./publico'));
 
+// Middleware de autenticação para as páginas protegidas
+app.use((requisicao, resposta, next) => {
+    if (!requisicao.session.autenticado) {
+        resposta.redirect('/login.html');
+    } else {
+        next();
+    }
+});
 
+// Direcionando as páginas protegidas para o diretório privado
+app.use("/privado", express.static("./privado"));
 
-app.use(autenticar, express.static("./privado"));
+// Para permitir o acesso às páginas privadas
+app.get("/privado/detalhes.html", (requisicao, resposta) => {
+    resposta.sendFile(__dirname + "/privado/detalhes.html");
+});
+
+app.get("/privado/cadastro.html", (requisicao, resposta) => {
+    resposta.sendFile(__dirname + "/privado/cadastro.html");
+});
 
 app.listen(porta, localhost, () => {
     console.log(`Servidor rodando em http://${localhost}:${porta}`);
